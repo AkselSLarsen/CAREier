389472a8e97f6ecac88092b5b3d7adb2a83e257d
AkselSLarsen <akse2669@edu.easj.dk>
2020-11-24 13:21:06 +0100

    Added and implemented ProductSorter. Added the skeleton IBuyer, IBringer and DetailedProduct.

---
+++ CAREier/Interfaces/IBringer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CAREier.Interfaces {
+    public interface IBringer {
+
+        public string Name { get; }
+    }
+}
---
+++ CAREier/Pages/DetailedProduct.cshtml.cs
@@ -0,0 +1,16 @@
+using CAREier.Interfaces;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CAREier.Pages {
+    public class DetailedProductModel : PageModel {
+
+        DetailedProductModel(IHandler<IProduct> handler) {
+
+        }
+
+        public void OnGet() {
+
+
+        }
+    }
+}
