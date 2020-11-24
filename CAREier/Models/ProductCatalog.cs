d0184057e5d5b0fd6712d850246e42bea29b2b65
Christoffer With <chri77h4@edu.easj.dk>
2020-11-24 13:17:42 +0100

    created product and productcatalog class and filled them out,

--- CAREier/Models/ProductCatalog.cs
+++ CAREier/Models/ProductCatalog.cs
@@ -1,21 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Threading.Tasks;
 using CAREier.Interfaces;
+using CAREier.Localizers;
 
 namespace CAREier.Models
 {
     public class ProductCatalog : IHandler<IProduct>
     {
+        private List<IProduct> _products;
+
+        public ProductCatalog(List<IProduct> products)
+        {
+            _products = new List<IProduct>();
+            products.Add(new Product("milk", 20, 1, new Vector3(1, 1, 1), new List<string>() ));
+        }
+
+        public List<IProduct> Products
+        {
+            get { return _products;}
+        }
         public void Create(IProduct item)
         {
-            throw new NotImplementedException();
+            Products.Add(item);
         }
 
         public IProduct Read()
         {
-            throw new NotImplementedException();
+            return Products;
         }
 
         public IProduct Update(IProduct pre, IProduct post)
