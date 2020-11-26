46cbef7dd6fef993cc703d040838ada587866975
GitG0at <rgo2@outlook.dk>
2020-11-26 14:01:25 +0100

    Out Commeted error

--- CAREier/Startup.cs
+++ CAREier/Startup.cs
@@ -22,7 +22,7 @@ namespace CAREier {
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddRazorPages();
-            services.AddSingleton<IHandler<IProduct>, ProductCatalog>();
+            //services.AddSingleton<IHandler<IProduct>, ProductCatalog>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
