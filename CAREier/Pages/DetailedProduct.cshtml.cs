7a0fd9b21b8c5abf8acd11ce5acffcbe2744eefa
AkselSLarsen <akse2669@edu.easj.dk>
2020-11-26 12:02:01 +0100

    Added comments to the IHandler metods.

--- CAREier/Pages/DetailedProduct.cshtml.cs
+++ CAREier/Pages/DetailedProduct.cshtml.cs
@@ -1,13 +1,15 @@
 using CAREier.Interfaces;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 
 namespace CAREier.Pages {
     public class DetailedProductModel : PageModel {
 
-        
+        private List<IProduct> products { get; set; }
+        private IProduct product { get; set; }
 
-        DetailedProductModel() {
-            
+        DetailedProductModel(IHandler<IProduct> handler) {
+            products = handler.ReadAll();
         }
 
         public void OnGet(int id) {
