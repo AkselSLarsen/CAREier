f17c89957ed3913903e981cb12d1e7333af8dd01
AkselSLarsen <akse2669@edu.easj.dk>
2020-11-30 10:12:33 +0100

    Fixed Login razor page.

---
+++ CAREier/Models/UserType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CAREier.Models {
+    public enum UserType {
+        Buyer,
+        Bringer,
+        Store
+    }
+}
--- CAREier/Pages/Login.cshtml.cs
+++ CAREier/Pages/Login.cshtml.cs
@@ -1,16 +1,25 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using CAREier.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace CAREier.Pages
-{
-    public class LoginModel : PageModel
-    {
-        public void OnGet()
-        {
+namespace CAREier.Pages {
+    public class LoginModel : PageModel {
+
+
+
+        public void OnGet(int id) {
+            switch(id) {
+                case 0: //UserType = Buyer
+                    
+                    break;
+                case 1: //UserType = Bringer
+
+                    break;
+                case 2: //UserType = Store
+
+                    break;
+
+
+            }
         }
     }
 }
