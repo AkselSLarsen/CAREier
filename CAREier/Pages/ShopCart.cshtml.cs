beb71db3c835bfb9486163a67a4238dd168de493
GitG0at <rgo2@outlook.dk>
2020-12-08 14:19:06 +0100

    Updatedede CSS and begun makeing shopping card

--- CAREier/Pages/ShopCart.cshtml.cs
+++ CAREier/Pages/ShopCart.cshtml.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CAREier.Helpers;
 using CAREier.Interfaces;
+using CAREier.Models;
 using CAREier.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -11,30 +13,42 @@ namespace CAREier.Pages
 {
     public class ShopCardModel : PageModel
     {
+        private ICRUD<Product> _products;
+        public List<Product> Products { get; set; }
         public ShoppingCartService ChartService { get; }
         public List<IOrder> Orders { get; set; }
-    
-        public ShopCardModel()
+        public List<string> Tags { get; set; }
+
+        public ShopCardModel(ICRUD<Product> products)
         {
-            //repo = repository;
-            //ChartService = chart;
-            //OrderedBooks = new List<Book>();
+            _products = products;
+            Products = new List<Product>();
+            foreach (var var in _products.ReadAll())
+            {
+                if (var is Product)
+                {
+                    Products.Add((Product)var);
+                }
+            }
         }
         public void OnGet()
         {
-            // Book book = repo.GetBook(isbn);
-            //ChartService.Add(book);
-            //OrderedBooks = ChartService.GetOrderedBooks();
-            //return Page();
+            Products = _products.ReadAll();
         }
 
-        public IActionResult OnPostDelete(string isbn)
+        public void OnPost()
         {
-            //ChartService.RemoveBook(isbn);
-
-            //OrderedBooks = ChartService.GetOrderedBooks();
-            return Page();
+           /* Products = ProductSorter.GetProductsWithTags(_products.ReadAll(), Tags);
+            if (Products.Count == 0)
+            {
+                Products = _products.ReadAll();
+            }*/
         }
-      
+
+        public string FormalizeTags(List<string> tags)
+        {
+            return TagFormalizer.TagsToString(tags);
+        }
+
     }
 }
