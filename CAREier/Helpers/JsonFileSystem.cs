4d2f2fadf8a590b89b1a9947157d53078456ebb6
GitG0at <rgo2@outlook.dk>
2020-12-14 12:36:20 +0100

    error

--- CAREier/Helpers/JsonFileSystem.cs
+++ CAREier/Helpers/JsonFileSystem.cs
@@ -9,67 +9,6 @@ using System.Threading.Tasks;
 
 namespace CAREier.Helpers {
     public static class JsonFileSystem {
-        //Read and Write Products
-        public static List<Product> ReadProduct(string JsonFileName) {
-            string jsonString = File.ReadAllText(JsonFileName);
-            return JsonConvert.DeserializeObject<List<Product>>(jsonString);
-        }
-            {
-                temp[i].id = i;
-            }
-            return temp;
-        }
-        public static void WriteProduct(List<Product> products, string JsonFileName) {
-        //Read and Write Orders
-        public static List<Order> ReadOrder(string JsonFileName) {
-
-            File.WriteAllText(JsonFileName, output);
-        }
-
-        public static List<Order> ReadOrder(string JsonFileName, ICRUD<Store> store, ICRUD<> store, ICRUD<Product> Bringer, ICRUD<Product> Prods) {
-            string jsonString = File.ReadAllText(JsonFileName);
-
-            return JsonConvert.DeserializeObject<List<Order>>(jsonString);
-        }
-        public static void WriteOrder(List<Order> orders, string JsonFileName) {
-            string output = Newtonsoft.Json.JsonConvert.SerializeObject(orders, Newtonsoft.Json.Formatting.Indented);
-
-            File.WriteAllText(JsonFileName, output);
-        }
-
-        public static List<Buyer> ReadBuyers(string JsonFileName)
-        {
-            string jsonString = File.ReadAllText(JsonFileName);
-            return JsonConvert.DeserializeObject<List<Buyer>>(jsonString);
-        }
-        public static void WriteBuyers(List<Buyer> buyers, string JsonFileName)
-        {
-            string output = Newtonsoft.Json.JsonConvert.SerializeObject(buyers, Newtonsoft.Json.Formatting.Indented);
-
-            File.WriteAllText(JsonFileName, output);
-        }
-
-        public static List<Store> ReadStores(string JsonFileName)
-        {
-            string jsonString = File.ReadAllText(JsonFileName);
-            return JsonConvert.DeserializeObject<List<Store>>(jsonString);
-        }
-        public static void WriteStores(List<Store> stores, string JsonFileName)
-        {
-            string output = Newtonsoft.Json.JsonConvert.SerializeObject(stores, Newtonsoft.Json.Formatting.Indented);
-
-            File.WriteAllText(JsonFileName, output);
-        }
-        public static List<Bringer> ReadBringers(string JsonFileName)
-        {
-            string jsonString = File.ReadAllText(JsonFileName);
-            return JsonConvert.DeserializeObject<List<Bringer>>(jsonString);
-        }
-        public static void WriteBringers(List<Bringer> bringers, string JsonFileName)
-        {
-            string output = Newtonsoft.Json.JsonConvert.SerializeObject(bringers, Newtonsoft.Json.Formatting.Indented);
-
-            File.WriteAllText(JsonFileName, output);
-        }
+      
     }
 }
